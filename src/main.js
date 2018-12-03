3795ebc0ee0823abf55952ff8853ee3445a510db
Daniel Bernhardt <16192489+BernhardtD@users.noreply.github.com>
2018-12-03 06:20:53 +0100

    Remove lodash dependency. Add readme content.

--- src/main.js
+++ src/main.js
@@ -1,9 +1,7 @@
 import Vue from "vue";
-import _ from "lodash";
 import SortedTablePlugin from "./index.js";
 import App from "./App.vue";
 
-Vue.prototype.$_ = _;
 Vue.use(SortedTablePlugin, {
   ascIcon: "<span> up</span>",
   descIcon: "<span> down</span>"
